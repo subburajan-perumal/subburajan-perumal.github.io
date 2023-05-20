e693bad5fc9da930b97b91382389a0e85dad6906
subburajan-dev <subburajanperumal@gmail.com>
2023-05-21 01:28:43 +0530

    update

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@
 const path = require('path');
 
 const nextConfig = {
+  output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   experimental: {
     appDir: true,
   },
+  
 };
 
 module.exports = nextConfig;
+z
