2665ee2703b6d93a35b397cc96224142a8c2ea9d
subburajan-dev <subburajanperumal@gmail.com>
2023-05-24 22:40:06 +0530

    fix: resume host

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -49,7 +49,7 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <Link href="20230521-Resume.pdf" className={styles.profile__link} download>Download CV</Link>
+        <a href="20230521-Resume.pdf" className={styles.profile__link} download>Download CV</a>
       </div>
 
 
